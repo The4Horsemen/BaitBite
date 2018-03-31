76be441e303dfe7614641033557fb03d6d06f9a9
janbisama <janbisma1@gmail.com>
2018-03-31 23:08:55 +0300

    Integration Complete

--- seniorProject/BaitBite/app/src/main/java/com/example/android/baitbite/Model/Customer.java
+++ seniorProject/BaitBite/app/src/main/java/com/example/android/baitbite/Model/Customer.java
@@ -1,25 +1,92 @@
 package com.example.android.baitbite.Model;
 
-public class Customer {
-    private String Name;
-    private String Password;
-    private String Phone;
+import java.util.UUID;
 
+public class Customer {
+
+    //private String Account_ID;
+    private String Email;
+    private int LocationX;
+    private int LocationY;
+    private int Mobile_Verification_Code;
+    private String Name;
+    private String Phone;
+    // delete this part after testing
+    private String Password;
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+    public String getPassword() {
+        return Password;
+    }
+
+    //..........................................
     public Customer() {
     }
 
-    public Customer(String name, String password, String phone) {
+    public Customer(String Name) {
+        this.Name = Name;
+    }
+
+    public Customer(String name, String phone) {
         Name = name;
-        Password = password;
         Phone = phone;
     }
 
-    public String getPhone() {
-        return Phone;
+    public Customer(String email, int locationX, int locationY, String name, String phone){
+        this.Email = email;
+        this.LocationX = locationX;
+        this.LocationY = locationY;
+        this.Name = name;
+        this.Phone = phone;
+
     }
 
-    public void setPhone(String phone) {
-        Phone = phone;
+
+
+    public void Get_GPS_Location() {
+
+
+
+
+    }
+
+
+
+
+
+
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public int getLocationX() {
+        return LocationX;
+    }
+
+    public void setLocationX(int locationX) {
+        LocationX = locationX;
+    }
+    public int getLocationY() {
+        return LocationY;
+    }
+
+    public void setLocationY(int locationY) {
+        LocationY = locationY;
+    }
+
+    public int getMobile_Verification_Code() {
+        return Mobile_Verification_Code;
+    }
+
+    public void setMobile_Verification_Code(int mobile_Verification_Code) {
+        Mobile_Verification_Code = mobile_Verification_Code;
     }
 
     public String getName() {
@@ -30,11 +97,20 @@ public class Customer {
         Name = name;
     }
 
-    public String getPassword() {
-        return Password;
+    public String getPhone() {
+        return Phone;
     }
 
-    public void setPassword(String password) {
-        Password = password;
+    public void setPhone(String phone) {
+        Phone = phone;
     }
+
+
+
+
+
+
+
+
+
 }
--- seniorProject/BaitBite/app/src/main/java/com/example/android/baitbite/SignUpActivity.java
+++ seniorProject/BaitBite/app/src/main/java/com/example/android/baitbite/SignUpActivity.java
@@ -7,21 +7,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+//import com.example.android.baitbite.GPS.GPSTracker;
 import com.example.android.baitbite.Model.Customer;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.karan.churi.PermissionManager.PermissionManager;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    //private GPSTracker gpsTracker;
     MaterialEditText editPhone, editName, editPassword;
-
+    protected PermissionManager permissionnManager;
     //Button SignUpActivity in SignUpActivity page
     Button buttonSignUp;
-
+    Button buttonLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +31,6 @@ public class SignUpActivity extends AppCompatActivity {
 
         editPhone = (MaterialEditText) findViewById(R.id.editPhone);
         editName = (MaterialEditText) findViewById(R.id.editName);
-        editPassword = (MaterialEditText) findViewById(R.id.editPassword);
-
         buttonSignUp = (Button) findViewById(R.id.buttonSignUp);
 
         //Init Firebase
@@ -43,7 +43,6 @@ public class SignUpActivity extends AppCompatActivity {
                 final ProgressDialog mDialog = new ProgressDialog(SignUpActivity.this);
                 mDialog.setMessage("Please wait...");
                 mDialog.show();
-
                 table_customer.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
@@ -54,7 +53,7 @@ public class SignUpActivity extends AppCompatActivity {
                             Toast.makeText(SignUpActivity.this, "The phone number is already registered by a customer", Toast.LENGTH_LONG).show();
                         }else {
                             mDialog.dismiss();
-                            Customer customer = new Customer(editName.getText().toString(), editPassword.getText().toString(), editPhone.getText().toString());
+                            Customer customer = new Customer(editName.getText().toString(), editPhone.getText().toString());
                             table_customer.child(editPhone.getText().toString()).setValue(customer);
                             Toast.makeText(SignUpActivity.this, "Sign up successfully !", Toast.LENGTH_LONG).show();
                             finish();
@@ -70,5 +69,40 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 
+                /*Aseel Code
+
+
+
+        buttonLocation.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0)
+            {
+                permissionnManager = new PermissionManager() {
+                };
+                permissionnManager.checkAndRequestPermissions(SignUpActivity.this);
+                gpsTracker = new GPSTracker(SignUpActivity.this);
+
+                // Eger konum bilgisi alinabiliyorsa ekranda goruntulenir
+                if (gpsTracker.canGetLocation())
+                {
+                    double latitude = gpsTracker.getLatitude();
+                    double longitude = gpsTracker.getLongitude();
+
+                    Toast.makeText(getApplicationContext(), "Location : \nX " + latitude + "\nY " + longitude, Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+
+                    gpsTracker.showSettingsAlert();
+                }
+            }
+        });
+
+
+
+
+
+                 End of Aseel Code*/
     }
 }
